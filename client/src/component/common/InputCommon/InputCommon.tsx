6c9a48377a3b02e93f4e6f2d28c65c8c9f2ed518
nj26-thucntd <nguyentrandoanthuc1212@gmail.com>
2023-03-09 13:15:15 +0700

    hide&show pass icon

--- client/src/component/common/InputCommon/InputCommon.tsx
+++ client/src/component/common/InputCommon/InputCommon.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Styles from "./InputCommon.module.css";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const InputCommon: React.FC<IPropsInputCommon> = (props) => {
   const {
@@ -18,23 +19,28 @@ const InputCommon: React.FC<IPropsInputCommon> = (props) => {
     <>
       <div className={Styles.input_group}>
         {label && <span className="input-group-text">{label}:</span>}
-        <input
-          type={isInputPassword && !isShowPassword ? "password" : type}
-          aria-label="Input field"
-          className={`form-control ${Styles.input_text}`}
-          style={{
-            color: color,
-          }}
-        />
-        {isInputPassword && (
-          <span
-            onClick={handleShowPassword}
-            style={{ color: isShowPassword ? "red" : "blue" }}
-          >
-            {isShowPassword ? "Hide" : "Show"}
-            {/* <i className={`bi bi-eye${isShowPassword ? "-slash" : ""}`} /> */}
-          </span>
-        )}
+        <div className={Styles.input}>
+          <input
+            type={isInputPassword && !isShowPassword ? "password" : type}
+            aria-label="Input field"
+            className={`form-control ${Styles.input_text}`}
+            style={{
+              color: color,
+            }}
+          />
+          {isInputPassword && (
+            <span
+              onClick={handleShowPassword}
+              style={{
+                color: isShowPassword ? "red" : "blue",
+                fontSize: "19px",
+                marginLeft: "3px",
+              }}
+            >
+              {isShowPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </span>
+          )}
+        </div>
       </div>
     </>
   );
